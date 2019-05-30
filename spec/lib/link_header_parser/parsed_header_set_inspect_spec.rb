29549e1313b24ea501d1297cb134e8312eefac8b
Jason Garber <jason@sixtwothree.org>
2019-05-30 14:21:03 -0400

    Update inspect spec

--- spec/lib/link_header_parser/parsed_header_set_inspect_spec.rb
+++ spec/lib/link_header_parser/parsed_header_set_inspect_spec.rb
@@ -2,6 +2,6 @@ describe LinkHeaderParser::ParsedHeaderSet, :inspect do
   subject(:parsed_header) { described_class.new(['</>; rel="home"'], base: 'https://example.com') }
 
   it 'returns a String' do
-    expect(parsed_header.inspect).to match(/^#<LinkHeaderParser::ParsedHeaderSet:0x[a-f0-9]+ @headers=\".*\">$/)
+    expect(parsed_header.inspect).to match(/^#<LinkHeaderParser::ParsedHeaderSet:0x[a-f0-9]+ @headers=\[.*\]>$/)
   end
 end
