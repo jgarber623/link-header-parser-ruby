7ec9d3f43651898b82d2714ba8cd0106f6ff0e03
Jason Garber <jason@sixtwothree.org>
2019-05-30 10:34:11 -0400

    Update regexp matching

--- spec/lib/link_header_parser/parsed_header_inspect_spec.rb
+++ spec/lib/link_header_parser/parsed_header_inspect_spec.rb
@@ -2,6 +2,6 @@ describe LinkHeaderParser::ParsedHeader, :inspect do
   subject(:parsed_header) { described_class.new('</>; rel="home"', base: 'https://example.com') }
 
   it 'returns a String' do
-    expect(parsed_header.inspect).to match(/^#<LinkHeaderParser::ParsedHeader:0x[a-f0-9]{12} @header=\".*\">$/)
+    expect(parsed_header.inspect).to match(/^#<LinkHeaderParser::ParsedHeader:0x[a-f0-9]+ @header=\".*\">$/)
   end
 end
