20ea56a2c113bf5f5ab4c5c4c578148ea6c57793
Jason Garber <jason@sixtwothree.org>
2019-05-30 10:14:58 -0400

    Refactor parsed_header specs

--- spec/lib/link_header_parser/parsed_header_spec.rb
+++ spec/lib/link_header_parser/parsed_header_spec.rb
@@ -1,8 +1,6 @@
 describe LinkHeaderParser::ParsedHeader do
-  let(:base_url) { 'https://example.com' }
-
   context 'when header is not a String' do
-    subject(:parsed_header) { described_class.new(1, base: base_url) }
+    subject(:parsed_header) { described_class.new(1, base: 'https://example.com') }
 
     it 'raises an ArgumentError' do
       expect { parsed_header }.to raise_error(LinkHeaderParser::ArgumentError, 'header must be a String (given Integer)')
@@ -14,14 +12,6 @@ describe LinkHeaderParser::ParsedHeader do
 
     it 'raises an ArgumentError' do
       expect { parsed_header }.to raise_error(LinkHeaderParser::ArgumentError, 'base must be a String (given Integer)')
-    end
-  end
-
-  ExampleLinkHeaders::EXAMPLE_LINK_HEADERS.each do |header, result|
-    it "parses the Link header (#{header})" do
-      parsed_header = described_class.new(header, base: base_url)
-
-      expect(parsed_header.to_h).to include(result)
     end
   end
 end
