c5c35ce0bfb81411d7f3248d3f556911405bb5ec
Jason Garber <jason@sixtwothree.org>
2019-05-30 12:08:24 -0400

    Update subject naming

--- spec/lib/link_header_parser/parsed_header_set_spec.rb
+++ spec/lib/link_header_parser/parsed_header_set_spec.rb
@@ -1,17 +1,17 @@
 describe LinkHeaderParser::ParsedHeaderSet do
   context 'when headers is not an Array' do
-    subject(:parsed_header) { described_class.new(1, base: 'https://example.com') }
+    subject(:parsed_header_set) { described_class.new(1, base: 'https://example.com') }
 
     it 'raises an ArgumentError' do
-      expect { parsed_header }.to raise_error(LinkHeaderParser::ArgumentError, 'headers must be an Array (given Integer)')
+      expect { parsed_header_set }.to raise_error(LinkHeaderParser::ArgumentError, 'headers must be an Array (given Integer)')
     end
   end
 
   context 'when base is not a String' do
-    subject(:parsed_header) { described_class.new(['</>; rel="home"'], base: 1) }
+    subject(:parsed_header_set) { described_class.new(['</>; rel="home"'], base: 1) }
 
     it 'raises an ArgumentError' do
-      expect { parsed_header }.to raise_error(LinkHeaderParser::ArgumentError, 'base must be a String (given Integer)')
+      expect { parsed_header_set }.to raise_error(LinkHeaderParser::ArgumentError, 'base must be a String (given Integer)')
     end
   end
 end
