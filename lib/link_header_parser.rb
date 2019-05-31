e03e039ed16cd808e5358e429a94197de2696c8b
Jason Garber <jason@sixtwothree.org>
2019-05-30 23:20:03 -0400

    Add parse class method

--- lib/link_header_parser.rb
+++ lib/link_header_parser.rb
@@ -7,3 +7,11 @@ require 'link_header_parser/exceptions'
 
 require 'link_header_parser/parsed_header'
 require 'link_header_parser/parsed_header_collection'
+
+module LinkHeaderParser
+  class << self
+    def parse(*headers, base:)
+      ParsedHeaderCollection.new(headers, base: base)
+    end
+  end
+end
