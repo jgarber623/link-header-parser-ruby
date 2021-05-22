c7c8578fe985a8dd4c90e2752b69d3c34ce1b08d
Jason Garber <jason@sixtwothree.org>
2021-05-21 23:36:00 -0400

    Update specs

--- spec/lib/link_header_parser/link_header_spec.rb
+++ spec/lib/link_header_parser/link_header_spec.rb
@@ -2,16 +2,16 @@ describe LinkHeaderParser::LinkHeader do
   context 'when header is not a String' do
     subject(:link_header) { described_class.new(1, base: 'https://example.com') }
 
-    it 'raises an ArgumentError' do
-      expect { link_header }.to raise_error(LinkHeaderParser::ArgumentError, 'field_value must be a String (given Integer)')
+    it 'raises a NoMethodError' do
+      expect { link_header }.to raise_error(NoMethodError)
     end
   end
 
   context 'when base is not a String' do
     subject(:link_header) { described_class.new('</>; rel="home"', base: 1) }
 
-    it 'raises an ArgumentError' do
-      expect { link_header }.to raise_error(LinkHeaderParser::ArgumentError, 'base must be a String (given Integer)')
+    it 'raises a NoMethodError' do
+      expect { link_header }.to raise_error(NoMethodError)
     end
   end
 end
