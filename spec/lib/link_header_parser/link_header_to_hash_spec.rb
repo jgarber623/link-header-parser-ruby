6dd9b1e91ad91d2d548f256f91de692173cbbfed
Jason Garber <jason@sixtwothree.org>
2022-01-06 09:58:53 -0500

    Add anchor/context specs

--- spec/lib/link_header_parser/link_header_to_hash_spec.rb
+++ spec/lib/link_header_parser/link_header_to_hash_spec.rb
@@ -10,4 +10,14 @@ describe LinkHeaderParser::LinkHeader, '#to_hash' do
       end
     end
   end
+
+  SpecFixtures.example_link_headers_with_anchors.each do |header, result|
+    context "when header is #{header}" do
+      subject(:link_header) { described_class.new(header, base: 'https://example.com') }
+
+      it 'returns a Hash' do
+        expect(link_header.to_hash).to include(result)
+      end
+    end
+  end
 end
