80c80dd4a00f1bcbb1a92a0c9cadf5e04fbdd87b
Jason Garber <jason@sixtwothree.org>
2022-01-06 14:19:28 -0500

    Satisfy RuboCop

--- spec/lib/link_header_parser/link_headers_collection_inspect_spec.rb
+++ spec/lib/link_header_parser/link_headers_collection_inspect_spec.rb
@@ -4,6 +4,8 @@ describe LinkHeaderParser::LinkHeadersCollection, '#inspect' do
   subject(:link_header) { described_class.new(['</>; rel="home"'], base: 'https://example.com') }
 
   it 'returns a String' do
-    expect(link_header.inspect).to match(/^#<LinkHeaderParser::LinkHeadersCollection:0x[a-f0-9]+ headers: \[.*\], relation_types: \[.*\]>$/)
+    regexp = /^#<LinkHeaderParser::LinkHeadersCollection:0x[a-f0-9]+ headers: \[.*\], relation_types: \[.*\]>$/
+
+    expect(link_header.inspect).to match(regexp)
   end
 end
