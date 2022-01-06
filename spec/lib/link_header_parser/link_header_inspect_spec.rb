3e38706b409463e27b0aecf01dc6acf1d335e9e7
Jason Garber <jason@sixtwothree.org>
2022-01-06 10:36:14 -0500

    Revert "Use multi-line regexp for better readability"
    This reverts commit 7af4388e1cc187321db44921bdabb7fe9375811d.

--- spec/lib/link_header_parser/link_header_inspect_spec.rb
+++ spec/lib/link_header_parser/link_header_inspect_spec.rb
@@ -4,10 +4,6 @@ describe LinkHeaderParser::LinkHeader, '#inspect' do
   subject(:link_header) { described_class.new('</>; rel="home"', base: 'https://example.com') }
 
   it 'returns a String' do
-    expect(link_header.inspect).to match(/
-        ^#<LinkHeaderParser::LinkHeader:0x[a-f0-9]+\s
-        target_uri: ".*",\s
-        relation_types: \[.*\]>$
-      /x)
+    expect(link_header.inspect).to match(/^#<LinkHeaderParser::LinkHeader:0x[a-f0-9]+ target_uri: ".*", relation_types: \[.*\]>$/)
   end
 end
--- spec/lib/link_header_parser/link_header_parameter_inspect_spec.rb
+++ spec/lib/link_header_parser/link_header_parameter_inspect_spec.rb
@@ -4,10 +4,6 @@ describe LinkHeaderParser::LinkHeaderParameter, '#inspect' do
   subject(:link_parameter) { described_class.new('rel="home"') }
 
   it 'returns a String' do
-    expect(link_parameter.inspect).to match(/
-        ^#<LinkHeaderParser::LinkHeaderParameter:0x[a-f0-9]+\s
-        name: ".*",\s
-        value: ".*">$
-      /x)
+    expect(link_parameter.inspect).to match(/^#<LinkHeaderParser::LinkHeaderParameter:0x[a-f0-9]+ name: ".*", value: ".*">$/)
   end
 end
